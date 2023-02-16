73f7dfc778b06eb2fe0d6d4989e54586089a9e98
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2023-02-17 00:19:08 +0530

    Add files via upload

---
+++ Files For Project/Components/about.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './navbar';
+import './style.css';
+import './about.css';
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />      
+
+      <div id="about1">
+        <div id="about2">
+            <div id="about3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolorum, saepe cum rem architecto necessitatibus numquam repellat quisquam doloribus, molestias nemo sapiente. Reiciendis in cumque, reprehenderit veritatis sint tempore voluptatem praesentium tenetur illo alias nesciunt esse architecto similique temporibus totam deleniti perspiciatis? Rem suscipit illum, dolorum facilis assumenda, necessitatibus illo perferendis eaque nostrum, quod iste harum ipsa aspernatur quidem eos consequuntur maiores et soluta id adipisci molestias laborum ipsam? Quibusdam fugiat impedit laudantium adipisci? Non reprehenderit suscipit aliquam maiores illum tempore, atque incidunt optio reiciendis in id. Inventore repudiandae eius sapiente? Maiores, quod atque? Sed quia hic molestias saepe eos.
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default About
